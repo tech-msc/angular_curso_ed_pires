38d0f7905a6178f7afc310c2ef8b29b350047eab
Matheus S. Cardoso <matheusscardoso5@gmail.com>
2020-06-17 00:24:08 -0300

    modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,18 @@ import { APP_BASE_HREF } from '@angular/common';
 import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { MenuComponent } from './navegacao/menu/menu.component';
-import { HomeComponent } from './navegacao/home/home.component';
-import { FooterComponent } from './navegacao/footer/footer.component';
+
 import { SobreComponent } from './institucional/sobre/sobre.component';
-import { rootRouterConfig } from './app.routes';
 import { CadastroComponent } from './demos/reactive-forms/cadastro/cadastro.component';
 import { NgBrazil } from 'ng-brazil' 
 import { TextMaskModule } from 'angular2-text-mask';
 import { CustomFormsModule } from 'ng2-validation'
+import { NavegacaoModule } from './navegacao/navegacao.module';
+import { AppRoutingModule } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
-    HomeComponent,
-    FooterComponent,
     SobreComponent,
     CadastroComponent
   ],
@@ -31,7 +27,8 @@ import { CustomFormsModule } from 'ng2-validation'
     NgBrazil,
     TextMaskModule,
     CustomFormsModule,
-    [RouterModule.forRoot(rootRouterConfig, { useHash: false})]
+    NavegacaoModule,    
+    AppRoutingModule
   ],
   providers: [
     {provide: APP_BASE_HREF, useValue: '/'}
