6e04d38153ea28697d19cdc6bd5d5884014cbf94
tech-msc <23535116+tech-msc@users.noreply.github.com>
2020-06-18 23:18:07 -0300

    Input decorator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,15 @@ import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
 import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 
-import { AppComponent } from './app.component';
-
-import { SobreComponent } from './institucional/sobre/sobre.component';
-import { CadastroComponent } from './demos/reactive-forms/cadastro/cadastro.component';
-import { NgBrazil } from 'ng-brazil' 
-import { TextMaskModule } from 'angular2-text-mask';
+import { NgBrazil } from 'ng-brazil'
+import { TextMask } from 'ng-brazil';
 import { CustomFormsModule } from 'ng2-validation'
+
+import { AppComponent } from './app.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.component';
 import { NavegacaoModule } from './navegacao/navegacao.module';
+
 import { AppRoutingModule } from './app.routes';
 
 @NgModule({
@@ -24,10 +25,10 @@ import { AppRoutingModule } from './app.routes';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    NavegacaoModule,
+    TextMask.TextMaskModule,
     NgBrazil,
-    TextMaskModule,
     CustomFormsModule,
-    NavegacaoModule,    
     AppRoutingModule
   ],
   providers: [
