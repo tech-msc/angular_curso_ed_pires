91034028f403146b9f9e07d00ebe4d0ddd6001ff
Matheus S. Cardoso <matheusscardoso5@gmail.com>
2020-08-20 00:45:06 -0300

    link de navegação dinâmicos

--- src/app/navegacao/menu/menu.component.ts
+++ src/app/navegacao/menu/menu.component.ts
@@ -4,4 +4,51 @@ import { Component } from '@angular/core';
   selector: 'app-menu',
   templateUrl: './menu.component.html'
 })
-export class MenuComponent {}
+export class MenuComponent { 
+  
+  isadmin=true;
+  
+  nav: Nav[] = 
+  [
+    {
+      link: '/home',
+      name: 'Home',
+      exact: true,
+      admin: false
+    },
+    {
+      link: '/cadastro',
+      name: 'Cadastro',
+      exact: true,
+      admin: false
+    },
+    {
+      link: '/sobre',
+      name: 'Sobre',
+      exact: true,
+      admin: false
+    },
+    {
+      link: '/produtos',
+      name: 'Produtos',
+      exact: false,
+      admin: false
+    },
+    {
+      link: '/admin',
+      name: 'Admin',
+      exact: false,
+      admin: false
+    }
+  ];
+
+}
+
+
+
+interface Nav {
+link: string,
+name: string,
+exact: boolean,
+admin: boolean
+}
